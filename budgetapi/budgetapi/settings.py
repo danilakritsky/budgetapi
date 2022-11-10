bce34aa92f752c44551356aeb1d65301bc00b849
Danila Kritsky <danilakritsky@gmail.com>
2022-11-10 23:36:10 +0300

    Add categories app

--- budgetapi/budgetapi/settings.py
+++ budgetapi/budgetapi/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "users.apps.UsersConfig"
+    "users.apps.UsersConfig",
+    "categories.apps.CategoriesConfig"
 ]
 
 MIDDLEWARE = [
---
+++ budgetapi/categories/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+from django.db import models
+
+DEFAULT_CATEGORIES: list[str] = [
+    "Забота о себе",
+    "Зарплата",
+    "Здоровье и фитнес",
+    "Кафе и рестораны",
+    "Машина",
+    "Образование",
+    "Отдых и развлечения",
+    "Платежи, комиссии",
+    "Покупки: одежда, техника",
+    "Продукты",
+    "Проезд",
+]
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "categories"
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    category_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return (self.user, self.title)
+
