746e689b7f2a2aaa764d29fb09ce71e4bda166e4
Danila Kritsky <danilakritsky@gmail.com>
2022-11-12 01:55:22 +0300

    Add permissions to categories API

--- budgetapi/categories/urls.py
+++ budgetapi/categories/urls.py
@@ -4,6 +4,6 @@ from .views import CategoryDetail, CategoryList
 
 
 urlpatterns = [
-    path("<int:pk>/", CategoryDetail.as_view(), name="category_detail"),
+    path("/<int:pk>", CategoryDetail.as_view(), name="category_detail"),
     path("", CategoryList.as_view(), name="category_list"),
 ]
--- budgetapi/categories/views.py
+++ budgetapi/categories/views.py
@@ -1,14 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Category
 from .serializers import CategorySerializer
-
+from .permissions import IsAuthor
 
 class CategoryList(generics.ListCreateAPIView):
-    queryset = Category.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = CategorySerializer
 
+    def get_queryset(self):
+            user = self.request.user
+            return Category.objects.filter(user=user)
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthor,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
