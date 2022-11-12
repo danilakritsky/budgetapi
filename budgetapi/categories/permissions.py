fa291e2c70c3bf8b924afe4b87d549993251e981
Danila Kritsky <danilakritsky@gmail.com>
2022-11-12 18:58:57 +0300

    Update Category API permissions

--- budgetapi/categories/permissions.py
+++ budgetapi/categories/permissions.py
@@ -1,10 +1,14 @@
 from rest_framework import permissions
 
-class IsAuthor(permissions.BasePermission):
+class IsAuthenticatedAdminOrAuthor(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return True
         return False
 
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        if request.user.is_superuser or (obj.user == request.user):
+            return True
+        raise PermissionDenied(
+            {"message": "You don't have permission to access {obj.id}."}
+        )
--- budgetapi/categories/views.py
+++ budgetapi/categories/views.py
@@ -1,24 +1,34 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 
 from .models import Category
 from .serializers import CategorySerializer
-from .permissions import IsAuthor
+from .permissions import IsAuthenticatedAdminOrAuthor
 
 class CategoryList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (
+        IsAuthenticatedAdminOrAuthor,
+    )
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        user = self.request.user
-        return Category.objects.filter(user=user)
+        if self.request.user.is_superuser:
+            return Category.objects.all()
+        return Category.objects.filter(user=self.request.user)
     
     def post(self, request):
-        if request.data['user'] == self.request.user.id:
-            return Response(request.data)
-        return Response({})
+        if (request.data['user'] == self.request.user.id) or (
+            request.user.is_superuser
+        ):
+            serializer = CategorySerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+        raise PermissionDenied()
+
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthor,)
+    permission_classes = (IsAuthenticatedAdminOrAuthor,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
