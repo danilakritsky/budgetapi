645ef7d1bb67d7985237cddec1c09aa3219d934f
Danila Kritsky <danilakritsky@gmail.com>
2022-11-15 02:21:07 +0300

    Configure gunicorn and nginx

--- budgetapi/budgetapi/settings.py
+++ budgetapi/budgetapi/settings.py
@@ -21,14 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
+SECRET_KEY = os.getenv(
+    'BUDGETAPI_SECRET_KEY',
     "django-insecure-*(l7q3av)0!lq)^g#3!++%$l7=67@q5b+qp(a(4n%!a1l9^_-o"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.getenv('BUDGETAPI_ALLOWED_HOSTS') else True
 
-ALLOWED_HOSTS: list[str] = []
+# NOTE: https://www.divio.com/blog/django-allowed-hosts-explained/
+ALLOWED_HOSTS: list[str] = (
+    # provide a comma delimited list of allowed hosts 
+    os.getenv('BUDGETAPI_ALLOWED_HOSTS').replace(' ', "").split(',')
+    if os.getenv('BUDGETAPI_ALLOWED_HOSTS')
+    else []
+ )
 
 
 # Application definition
@@ -45,6 +52,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "corsheaders",
     "django_filters",
     "rest_framework.authtoken",
     "dj_rest_auth.registration",
@@ -57,12 +65,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 ROOT_URLCONF = "budgetapi.urls"
 
@@ -83,6 +93,22 @@ TEMPLATES = [
     },
 ]
 
+CORS_ALLOWED_ORIGINS = (
+    os.getenv('BUDGETAPI_CORS_ALLOWED_ORIGINS').replace(' ', "").split(',')
+    if os.getenv('BUDGETAPI_CORS_ALLOWED_ORIGINS')
+    else []
+ )
+
+# NOTE: https://github.com/adamchainz/django-cors-headers#csrf-integration
+CSRF_TRUSTED_ORIGINS = (
+    os.getenv('BUDGETAPI_CSRF_TRUSTED_ORIGINS').replace(' ', "").split(',')
+    if os.getenv('BUDGETAPI_CSRF_TRUSTED_ORIGINS')
+    else [
+        f"http://localhost:{os.getenv('BUDGETAPI_NGINX_PORT', '1337')}"
+    ]
+ )
+
+
 WSGI_APPLICATION = "budgetapi.wsgi.application"
 
 
@@ -92,11 +118,11 @@ WSGI_APPLICATION = "budgetapi.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB") or "budgetapi",
-        "USER": os.getenv("POSTGRES_USER") or "budgetapi",
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD") or "budgetapi",
-        "HOST": os.getenv("POSTGRES_HOST") or "postgres" or "localhost",
-        "PORT": os.getenv("POSTGRES_PORT") or "5432",
+        "NAME": os.getenv("BUDGETAPI_POSTGRES_DB", "budgetapi"),
+        "USER": os.getenv("BUDGETAPI_POSTGRES_USER", "budgetapi"),
+        "PASSWORD": os.getenv("BUDGETAPI_POSTGRES_PASSWORD", "budgetapi"),
+        "HOST": os.getenv("BUDGETAPI_POSTGRES_HOST") or "postgres" or "localhost",
+        "PORT": os.getenv("BUDGETAPI_POSTGRES_PORT", "5432"),
     }
 }
 
@@ -143,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -171,3 +197,5 @@ REST_FRAMEWORK = {
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # new
 SITE_ID = 1  # new
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
